0830666d098debf3cee8648b2b8e1c2619e18350
Codepheonix0 <108499830+Codepheonix0@users.noreply.github.com>
2023-06-15 22:15:44 -0500

    Create ProxyRotation.js

---
+++ ProxyRotation.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+var ip_addresses = [];
+var port_numbers = [];
+async function GetProxy()
+{
+const html = await axios.get("https://sslproxies.org/") 
+try{
+const $ = cheerio.load(html.data);
+
+    $("td:nth-child(1)").each(function(index, value) {
+      ip_addresses[index] = $(this).text();
+    });
+
+    $("td:nth-child(2)").each(function(index, value) {
+      port_numbers[index] = $(this).text();
+    });
+    
+  ip_addresses.join(", ");
+  port_numbers.join(", ");
+  let random_number = Math.floor(Math.random() * 100);
+    return `http://${ip_addresses[random_number]}:${port_numbers[random_number]}`
+
+  } catch(error)
+  {
+    console.log("Error loading proxy, please try again");
+  }
+
+
+}
+module.exports = {GetProxy}
